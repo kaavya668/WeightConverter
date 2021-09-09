ebeda3f330180346bd82dd2606be5cbbf9da7f11
kaavya668 <90324557+kaavya668@users.noreply.github.com>
2021-09-09 17:58:02 +0530

    Add files via upload

---
+++ scriptConverter.js
@@ -0,0 +1,9 @@
+function convert(){
+    var a=document.getElementById("input").value;
+    var p=a*2.2;
+    var g=a*1000;
+    var o=a*35.274;
+    document.getElementById("p1").innerHTML=p;
+    document.getElementById("o1").innerHTML=o;
+    document.getElementById("g1").innerHTML=g;
+}
